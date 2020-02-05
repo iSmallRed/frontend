709ebaac88af1aff6908745b6d3e6fde9858974d
zhangxuewen <944494038@example.com>
2020-02-05 16:06:47 +0800

    退出清除token提示

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -12,7 +12,7 @@
       </el-col>
       <el-col :span="2">
         <div class="grid-content bg-purple">
-          <a href="#" class="loginout">退出</a>
+          <a @click.prevent="handleSignout()" class="loginout">退出</a>
         </div>  
       </el-col>
     </el-row>
@@ -106,7 +106,18 @@ export default {
   // if token 有 -> 继续渲染组件
 
   // newVue 之前自动触发
+  },
+  methods:{
+    handleSignout(){
+      //  1.清除token
+      localStorage.clear()
+      //  2.提示
+      this.$message.success('退出成功')
+      //  3.来到login组件
+      this.$router.push({ name: 'login' })
+    }
   }
+
 }
 </script>
 
